05ae1d8e0507aeeeecfc53ab98e95c4732a36f7d
Nick Loeper <loepern@bc.edu>
2018-01-16 16:25:10 -0500

    Initial commit

---
+++ static/js/genres.js
@@ -0,0 +1,21 @@
+var GENRES = ['international', 'blues', 'jazz', 'classical', 'old-time/historic', 'country', 'pop',
+          'rock', 'easy listening', 'soul/rnb', 'electronic', 'folk', 'spoken', 'hip-hop', 'experimental', 'instrumental']
+
+var GENRE_TO_COLOR = {
+    'international': '#e6194b',
+    'blues': '#3cb44b',
+    'jazz': '#ffe119',
+    'classical': '#0082c8',
+    'old-time/historic': '#f58231',
+    'country': '#911eb4',
+    'pop': '#46f0f0',
+    'rock': '#f032e6',
+    'easy listening': '#aaffc3',
+    'soul/rnb': '#fabebe',
+    'electronic': '#008080',
+    'folk': '#e6beff',
+    'spoken': '#aa6e28',
+    'hip-hop': '#fffac8',
+    'experimental': '#800000',
+    'instrumental': '#808000',
+};
